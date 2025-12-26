20ac412df72a7fd2d75ed77cb91435e2342abded
Antoine Lafontaine <antoinelaf@gmail.com>
2025-12-26 14:52:10 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+import Regalon from "./components/Regalon";
+import Regalon2 from "./components/Regalon2";
+import Regalon3 from "./components/Regalon3";
+
+const App = () => {
+  return (
+    <div>
+      {/* <Regalon /> */}
+      {/* <Regalon2 /> */}
+      <Regalon3 />
+    </div>
+  );
+};
+
+export default App;
